1e6f12b96ed7bbaad5c51dd506f0e57a8db27ff9
rob cascella <github@packetfairy.com>
2016-01-29 23:56:02 -0800

    psuedo coding for how this might work - yes, i should probably be working in a branch.. but fuck it

--- simon.py
+++ simon.py
@@ -5,112 +5,144 @@ import random
 import time
 import sys
 
-red = 'r'
-yellow = 'y'
-blue = 'b'
-green = 'g'
-white = 'w'
-lightblue = 'l'
-brightblue = 'e'
-pink = 'k'
-brightred = 'd'
-purple = 'p'
+# color = [red,green,blue]
+# hoping that we can vary the luminosity of each diode so that I
+# can create more than just these seven colors.
+red =    [1, 0, 0]
+yellow = [1, 1, 0]
+white =  [1, 1, 1]
+purple = [1, 0, 1]
+blue =   [0, 0, 1]
+cyan =   [0, 1, 1]
+green =  [0, 1, 0]
 
+# where will this live?
 config = {}
-config['standard'] = { 'board': [ red, yellow, blue, green ],
+config['standard'] = { 'board': [red, yellow, blue, green],
                        'difficulty': 0 }
-config['brian'] = { 'board': [ blue, white, lightblue, brightblue ],
+config['brian'] = { 'board': [blue, white, cyan, purple],
                     'difficulty': 1 }
-config['bryan'] = { 'board': [ red, white, pink, brightred ] ,
+config['bryan'] = { 'board': [red, white, purple, white],
                     'difficulty': 2 }
-config['rob'] = { 'board': [ purple, red, purple, blue ] ,
+config['rob'] = { 'board': [purple, red, purple, blue],
                   'difficulty': 3 }
 
 # GPIO config details
-pin['red'] = 19
-pin['yellow'] = 13
-pin['blue'] = 6
-pin['green'] = 5
+LEDS = []
+# LEDS.append([red,green,blue])
+LEDS.append([4, 17, 27])
+LEDS.append([22, 5, 6])
+LEDS.append([13, 19, 26])
+LEDS.append([16, 20, 21])
+LED_SETUP = [pin for led_pins in LEDS for pin in led_pins]
 
-switch['red'] = 12
-switch['yellow'] = 16
-switch['blue'] = 20
-switch['green'] = 21
+SENSORS = [18, 23, 24, 25]
+
+RFID = 12
 
 
 def gpio_setup():
+    # will I get useful returncodes out of the GPIO if things are
+    # misconfigured or nonoperational, or do I need to trust that
+    # I have done the right things and that everything will always
+    # work perfectly? :)
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(switch.values(), GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.setup(pin.values(), GPIO.OUT)
+    GPIO.setup(LED_SETUP, GPIO.OUT)
+    GPIO.setup(SENSORS, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(RFID, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 
-def playcolor(color):
-    GPIO.output(pin[color], True)
-    time.sleep(0.5)
-    GPIO.output(pin[color], False)
+def playsound(sound):
+    """play sound file to audio port"""
+    print('you could try using subprocess, but that may be expensive.')
+    print('')
+    print('here are some links to help integrate audio directly into the')
+    print('script via python libraries for when you are ready!')
+    print('')
+    print('http://stackoverflow.com/questions/17657103/how-to-play-wav-file-in-python')
+    print('http://stackoverflow.com/questions/260738/play-audio-with-python')
+    print('https://wiki.python.org/moin/Audio/')
+    print('')
 
 
-def monitorswitches(seconds):
-    timeend = time.time() + seconds
-    while time.time() < timeend:
-        for s in switch:
-            if GPIO.input(s) == True:
-                return s
+def playcolor(color, sound):
+    GPIO.output(color, True)
+    playsound(sound)  # ensure sound file is sufficient length for LED to light
+    GPIO.output(color, False)
+
+
+def read_rfid_port():
+    # poll RFID port for presence of tag;
+    # if it exists, return its ID, else:
+    return 'standard'
+
+
+def read_sensor_ports():
+    for s in SENSORS:
+        if GPIO.input(s) == True:
+            return SENSORS.index(s)
     return False
 
 
-def play(colors, count, board):
-    select = random.randint(0, len(board) - 1)
-    color = board[select]
-    colors.append(color)
-    for color in colors:
-        playcolor(color)
-    c = 0
-    print('enter the colors:')
-    for color in colors:
-        c += 1
-        # need to implement timer function
-        # hpoing that timer will be easy enough to "cheat" with
-        # something like this:
-        # expired = time.time() + some_growing_number_of_seconds
-        # while time.time() < expired:
-        #     gpio_port_response = check_gpio_ports()
-        #     if gpio_port_response:
-        #         if gpio_port_response != color:
-        #             return incorrect error and exit
-        #     time.sleep(0.01)
-        # else:
-        #     return timeout error and exit
-        #
-        # the alternative seems to be for the timer to live in a thread, but
-        # i think i am not fancy enough to know how to make that work yet.
-        user_input = getch()
-        if user_input in ['', '!']:
-            print('thanks for playing')
-            sys.exit(0)
-        if user_input != color:
-            print('wrong selection (%s, you chose: %s)' % (color, user_input))
-            return False, colors
-    # trigger sound play
-    #   - name files as USER_COUNT.mp3 for easy play
-    # when no more files remain, play some other file?
-    return True, colors
+def play(color_sequence, count, user):
+    # define sound file names
+    # for when the color is being shown to you
+    show_sound = '%s_%s_%s.wav' % (count, user, 'show')
+    # for when you select the correct color
+    play_sound = '%s_%s_%s.wav' % (count, user, 'play')
+    # for when you select the wrong color
+    fail_sound = '%s_%s_%s.wav' % (count, user, 'fail')
+    # for when your game ends
+    over_sound = '%s_%s_%s.wav' % (count, user, 'over')
+    # for when you pass the round
+    pass_sound = '%s_%s_%s.wav' % (count, user, 'pass')
 
-
-def rungame(user='standard'):
+    # pick a new color
     board = config[user]['board']
-    a = True
+    select = random.randint(0, len(board) - 1)
+    append_color = board[select]
+    color_sequence.append(append_color)
+
+    # display the colors for the user
+    for color in color_sequence:
+        playcolor(color, show_sound)
+
+    # set up a timer equal to 1 second plus a half second for every
+    # color after the first
+    expired = time.time() + ( 1 + (count * 0.5) )
+
+    # wait for the user to enter the correct sequence
+    for color in color_sequence:
+        while time.time() < expired:
+            sensor_port_response = read_sensor_ports()
+            if sensor_port_response:
+                if sensor_port_response != board.index(color):
+                    playcolor(color, fail_sound)
+                    return False, color_sequence
+                else:
+                    playcolor(color, play_sound)
+        else:
+            playsound(over_sound)
+            return False, color_sequence
+    playsound(pass_sound)
+    return True, color_sequence
+
+
+def rungame(user):
+    winning = True
     count = 0
-    colors = []
-    while a is True:
+    color_sequence = []
+    while winning is True:
         count += 1
-        (a, colors) = play(colors, count, board)
+        (winning, color_sequence) = play(color_sequence, count, user)
     else:
-        print('wah wah!')
+        print('wah wah! you made it %s rounds!' % count - 1)
         print()
 
 
 if __name__ == '__main__':
+    gpio_setup()
     while True:
         # if RFID receiver registers USER nearby, set and send user as arg
-        rungame()
+        user = read_rfid_port()
+        rungame(user)

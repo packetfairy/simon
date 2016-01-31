63e03ea7dc0c3df2bd8a28529deed338f9a51e36
rob cascella <github@packetfairy.com>
2016-01-30 19:06:37 -0800

    shoving this into the repo so i have the notes as a record

---
+++ watchdog.py
@@ -0,0 +1,19 @@
+# stolen from:
+# http://stackoverflow.com/questions/16148735/how-to-implement-a-watchdog-timer-in-python
+from threading import Timer
+
+class Watchdog:
+    def __init__(self, timeout, userHandler=None):  # timeout in seconds
+        self.timeout = timeout
+        self.handler = userHandler if userHandler is not None else self.defaultHandler
+        self.timer = Timer(self.timeout, self.handler)
+
+    def reset(self):
+        self.timer.cancel()
+        self.timer = Timer(self.timeout, self.handler)
+
+    def stop(self):
+        self.timer.cancel()
+
+    def defaultHandler(self):
+        raise self
